72b7f8ff014e309f09c475adb338cecde7110351
sahan-chinthaka <sahanchinthaka04@gmail.com>
2023-08-13 21:08:19 +0530

    User auth context admin checking functionality added.

--- src/Hooks/useUser.jsx
+++ src/Hooks/useUser.jsx
@@ -1,6 +1,7 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
-import { Auth } from "../firebase.init";
+import { Auth, FS } from "../firebase.init";
+import { doc, getDoc } from "firebase/firestore";
 
 const AuthContext = createContext();
 
@@ -9,11 +10,25 @@ export function useUser() {
 }
 
 export function AuthProvider({ children }) {
-	const [user, setUser] = useState({});
+	const [user, setUser] = useState({
+		data: undefined,
+		isAdmin: false,
+	});
 
 	useEffect(() => {
 		return onAuthStateChanged(Auth, (user) => {
-			console.log("User", user);
+			setUser({
+				data: user,
+				isAdmin: false,
+			});
+
+			if (user) {
+				const d = doc(FS, "users", user.uid);
+				getDoc(d).then((snap) => {
+					const data = snap.data();
+					setUser((u) => ({ ...u, isAdmin: data?.isAdmin ?? false }));
+				});
+			}
 		});
 	}, []);
 
